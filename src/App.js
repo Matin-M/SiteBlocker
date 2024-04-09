677c6a3778f2c747fb2ea71d585f6130eeff6f09
Matin Massoudi <matin@massoudi.net>
2024-04-08 22:44:06 -0700

    Refactor confirmation dialog

--- src/App.js
+++ src/App.js
@@ -6,13 +6,8 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import PropTypes from 'prop-types';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import BlockedSitesList from './components/BlockedSitesList';
+import ConfirmationDialog from './components/ConfirmationDialog';
 
 const theme = createTheme({
   breakpoints: {
@@ -40,52 +35,6 @@ const theme = createTheme({
     }
   }
 });
-
-const ConfirmationDialog = ({ open, onClose, onConfirm, siteToRemove }) => {
-  const [confirmText, setConfirmText] = useState('');
-
-  const handleConfirmClick = () => {
-    if (confirmText === siteToRemove) {
-      onConfirm();
-      setConfirmText('');
-    } else {
-      alert('The entered site does not match. Please try again.');
-    }
-  };
-
-  return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Confirm Removal</DialogTitle>
-      <DialogContent>
-        <DialogContentText>Type: {siteToRemove}</DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Site Name"
-          type="text"
-          fullWidth
-          variant="standard"
-          value={confirmText}
-          onChange={(e) => setConfirmText(e.target.value)}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={handleConfirmClick} autoFocus>
-          Yes, Remove
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-};
-
-ConfirmationDialog.propTypes = {
-  open: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
-  onConfirm: PropTypes.func.isRequired,
-  siteToRemove: PropTypes.string.isRequired
-};
 
 function App() {
   const [blockedSites, setBlockedSites] = useState([]);
