e01622a5063e30a33d5cac2c99d5859707cf6151
Matin Massoudi <matin@massoudi.net>
2024-04-24 20:18:07 -0700

    Updated YT blocking to use messaging

--- public/content-script.js
+++ public/content-script.js
@@ -1,5 +1,18 @@
+function getBrowser() {
+  if (typeof chrome !== 'undefined' && typeof chrome.runtime !== 'undefined') {
+    return chrome;
+  } else if (
+    typeof browser !== 'undefined' &&
+    typeof browser.runtime !== 'undefined'
+  ) {
+    return browser;
+  } else {
+    console.log('browser is not supported');
+    return false;
+  }
+}
+
 function handleDOMContentLoaded() {
-  console.log('Page content has loaded');
   const channelNameElement = document.querySelector('a.yt-simple-endpoint');
   const channelName = channelNameElement
     ? channelNameElement.textContent.trim()
@@ -8,11 +21,7 @@ function handleDOMContentLoaded() {
   chrome.storage.sync.get(['allowedChannels'], function (result) {
     const allowedChannels = result.allowedChannels || [];
     if (!allowedChannels.includes(channelName)) {
-      // set currrent tab to blocked.html
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const currentTab = tabs[0];
-        chrome.tabs.update(currentTab.id, { url: 'blocked.html' });
-      });
+      getBrowser().runtime.sendMessage({ action: 'blockPage' });
     } else {
       console.log(`Access granted to: ${channelName}`);
     }
